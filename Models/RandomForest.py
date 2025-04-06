349229b188cb212f1da711a4187fcf74fa51375f
fcostell0 <ftcostello@loyola.edu>
2025-04-05 22:03:12 -0400

    SVM

---
+++ Models/RandomForest.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report
+
+educationData = pd.read_csv('C:/Users/finco/Documents/GitHub/DS496Capstone/Processed Data/educationData.csv')
+raceData = pd.read_csv('C:/Users/finco/Documents/GitHub/DS496Capstone/Processed Data/raceData.csv')
+labels = pd.read_csv('C:/Users/finco/Documents/GitHub/DS496Capstone/Processed Data/electionLabels.csv')
+
+data = pd.merge(educationData, raceData, how='inner', on=['state_po', 'year', 'district'])
+data = pd.merge(data, labels, how='inner', on=['state_po', 'year', 'district'])
+
+y = data['republican_victory']
+X = data.drop(['state_po', 'year', 'district', 'republican_victory'], axis = 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=21)
+
+pipe_rf = Pipeline([('std', StandardScaler()), ('pca', PCA(n_components=0.95)), ('rf', RandomForestClassifier())])
+rf_param_grid = {
+    'rf__n_estimators':[100, 500, 1000],
+    'rf__criterion':['gini', 'entropy', 'log_loss'],
+    'rf__max_features':['sqrt', 'log2']
+}
+
+RF_gs = GridSearchCV(estimator=pipe_rf, param_grid=rf_param_grid, scoring='f1', refit=True, cv=10, verbose=3)
+
+RF_gs = RF_gs.fit(X_train, y_train)
+
+print("Best Random Forest Model: ")
+print("Training data F1 score: ", RF_gs.best_score_)
+print("Model parameters: ", RF_gs.best_params_)
+print()
+
+best_model = RF_gs.best_estimator_
+
+y_pred = best_model.predict(X_test)
+class_report = classification_report(y_test, y_pred)
+print(class_report)
